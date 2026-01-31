a298200a1be90b8b2d63c4e3377c43b5678ab8fc
tardis <tardis@tardis.com>
2026-01-31 11:19:48 +0800

    update

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import { paraglideMiddleware } from './paraglide/server.js'
+import handler from '@tanstack/react-start/server-entry'
+export default {
+	  fetch(req: Request): Promise<Response> {
+		      return paraglideMiddleware(req, ({ request }) => handler.fetch(request))
+		        },
+}
